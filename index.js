ebb4e94f3b395f281954f80a573002cba904ecec
PriyaG88 <priya.g71488@gmail.com>
2017-10-10 10:02:00 -0700

    Add artist and album imagery

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
 import AlbumList from './src/components/album_list';
 
+//expand component to fill entire content area of device
 const App = () => (
-  <View>
+  <View style={{ flex: 1}}>
     <Header headerText={'Albums'}/>
     <AlbumList />
   </View>
--- src/components/album_detail.js
+++ src/components/album_detail.js
@@ -1,16 +1,60 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './card';
 import CardSection from './card_section';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+  const { thumbnailStyle,
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailContainerStyle,
+    imageStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
-      </CardSection>  
+      <View style={thumbnailContainerStyle}>
+        <Image
+          style={thumbnailStyle}
+          source={{ uri: album.thumbnail_image}}/>
+      </View>
+      <View style={headerContentStyle}>
+        <Text style={headerTextStyle}>{album.title}</Text>
+        <Text>{album.artist}</Text>
+      </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: album.image} } />
+      </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
