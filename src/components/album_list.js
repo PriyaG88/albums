c7d37fe808c9269bea32af846b2d494faffede73
PriyaG88 <priya.g71488@gmail.com>
2017-10-09 23:55:08 -0700

    Add album detail view

--- src/components/album_list.js
+++ src/components/album_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import Axios from 'axios';
+import AlbumDetail from './album_detail';
 
 class AlbumList extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album =>
-      <Text key={album.title}>{album.title}</Text>
+      <AlbumDetail key={album.title} album={album} />
     );
   }
 
