16ff27e2d7374fb9be7b3b4d16f908c3eb2d72f5
PriyaG88 <priya.g71488@gmail.com>
2017-10-09 23:41:30 -0700

    Add album list view

--- src/components/album_list.js
+++ src/components/album_list.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: [] };
+  }
+  componentWillMount() {
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text>
+    );
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
